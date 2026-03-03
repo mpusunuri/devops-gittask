4e43531445664614b7e102001c4495185da3d20e
Ubuntu <ubuntu@ip-172-31-7-246.ap-south-1.compute.internal>
2026-03-03 06:37:22 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	  res.send("DevOps Git Task Running Successfully 🚀");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+	  console.log(`Server running on port ${PORT}`);
+});
